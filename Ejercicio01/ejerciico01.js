b9d14063fddd2d7751f606434c58f937a670e266
Alexander-Calle <ajcalle@sudamericano.edu.ec>
2023-12-21 09:59:11 -0500

    Added Ejercicio 1

--- Ejercicio01/ejerciico01.js
+++ Ejercicio01/ejerciico01.js
@@ -1,3 +1,23 @@
 `use strict`
 
+function MakeStudent(name, email, promedio) {
+  this.name = name,
+  this.email = email,
+  this.promedio = promedio
+  this.validarPromedio = function(){
+    if (this.promedio >= 70) {
+      console.log('Aprobado');
+    } else if(this.promedio <= 70) {
+      console.log('Reprobado');
+    }
+  }
+  this.getUser = function() {
+    let sep= this.email.indexOf("@");
+    let username = this.email.substring(0, sep);
+    console.log(username);
+  }
+}
 
+let User1 = new MakeStudent('Alexander', 'ajcalle@sudamericano.edu.ec', 85);
+User1.validarPromedio();
+User1.getUser();
